72a7f65495350cabca6209b0b5fc6cbfc77eb385
phelan2111 <110277335+phelan2111@users.noreply.github.com>
2024-09-15 18:31:29 +0700

    fix import

--- src/redux/slice/index.ts
+++ src/redux/slice/index.ts
@@ -1,21 +1,9 @@
 import { useAppSelector } from '@/hooks/redux';
-import { dataToolControl, funcToolControl } from './toolControl';
 import { dataLangue, funcLangue } from './langue';
-import { dataSong } from './song';
 import { dataModal, funcModal } from './modal';
-
-export const sliceToolControl = {
-	useGetState: () => useAppSelector(dataToolControl),
-	func: funcToolControl,
-};
 
 export const sliceLangue = {
 	useGetState: () => useAppSelector(dataLangue),
-	func: funcLangue,
-};
-
-export const sliceSong = {
-	useGetState: () => useAppSelector(dataSong),
 	func: funcLangue,
 };
 
