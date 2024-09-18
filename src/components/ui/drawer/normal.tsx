7f6fd2997c63c5b880c51ba19158036b29d9b570
Tan Dev <tan.lm@intelin.vn>
2024-09-18 18:08:21 +0700

    drawer: config drawer

--- src/components/ui/drawer/normal.tsx
+++ src/components/ui/drawer/normal.tsx
@@ -4,13 +4,100 @@ import MenuItem from '@/components/root/menu/item/normal';
 import {
 	GoBook,
 	GoChevronDown,
-	GoChevronRight,
 	GoCopilot,
-	GoHome,
 	GoStack,
+	GoChevronRight,
+	GoHome,
 } from 'react-icons/go';
+import { ReactNode, useMemo, useState } from 'react';
+import { Helper } from '@/utils/helper';
+import { PATH } from '@/routes/config';
+enum LAYER {
+	parents = 0,
+	child,
+}
+interface IItemDrawer {
+	icon?: ReactNode;
+	text: string;
+	id: string;
+	path: string;
+	children?: IItemDrawer[];
+	layer: LAYER;
+}
+const drawer: IItemDrawer[] = [
+	{
+		id: Helper.randomKey(),
+		path: PATH.HOME,
+		text: 'Home',
+		icon: <GoHome className='text-xl' />,
+		layer: LAYER.parents,
+	},
+	{
+		id: Helper.randomKey(),
+		path: PATH.HOME,
+		text: 'Music',
+		icon: <GoStack className='text-xl' />,
+		layer: LAYER.parents,
+		children: [
+			{
+				id: Helper.randomKey(),
+				path: PATH.HOME,
+				text: 'Album',
+				icon: <GoStack className='text-xl' />,
+				layer: LAYER.child,
+			},
+			{
+				id: Helper.randomKey(),
+				path: PATH.HOME,
+				text: 'Playlist',
+				icon: <GoStack className='text-xl' />,
+				layer: LAYER.child,
+			},
+		],
+	},
+	{
+		id: Helper.randomKey(),
+		path: PATH.HOME,
+		text: 'Artists',
+		icon: <GoCopilot className='text-xl' />,
+		layer: LAYER.parents,
+	},
+	{
+		id: Helper.randomKey(),
+		path: PATH.HOME,
+		text: 'Podcast',
+		icon: <GoBook className='text-xl' />,
+		layer: LAYER.parents,
+	},
+];
 
 function DrawerNormal() {
+	const [state, setState] = useState<[string, string]>(['', '']); // [parentsId, childrenId]
+
+	const idItemDrawerList: [string, string] = useMemo(
+		() => [...state],
+		[state],
+	);
+
+	const handleParentExist = (id: string) => {
+		const index = idItemDrawerList.findIndex((i) => i === id);
+		const isExist = index !== -1;
+		if (isExist) {
+			idItemDrawerList[0] = '';
+		} else {
+			idItemDrawerList[0] = id;
+		}
+	};
+	const handleClick = (dataItem: IItemDrawer) => {
+		const isParents = dataItem.layer === LAYER.parents;
+		if (isParents) {
+			handleParentExist(dataItem.id);
+		} else {
+			idItemDrawerList[1] = dataItem.id;
+		}
+		setState(idItemDrawerList);
+	};
+
 	return (
 		<aside className='h-full bg-white/10 backdrop-blur-2xl flex flex-col gap-4 rounded-l-2xl'>
 			<div className='p-4 h-20 overflow-hidden flex items-center justify-between'>
@@ -23,67 +110,50 @@ function DrawerNormal() {
 			</div>
 			<nav className='pr-3'>
 				<Menu>
-					<MenuItem>
-						<div className='flex items-center gap-2 text-md w-full rounded-2xl text-white bg-white/20 py-2 px-3 hover:bg-white/10 transition-colors duration-500'>
-							<GoHome className='text-xl' />
-							<div className='pt-1'>
-								<p>Home</p>
-							</div>
-						</div>
-					</MenuItem>
-					<MenuItem>
-						<div className='flex items-center justify-between gap-2 text-md w-full rounded-2xl text-white py-2 px-3 hover:bg-white/10 transition-colors duration-500'>
-							<div className='flex items-center gap-2'>
-								<GoStack className='text-xl' />
-								<div className='pt-1'>
-									<p>Music</p>
-								</div>
-							</div>
-							<GoChevronDown />
-						</div>
-						<Menu gap='gap-0'>
-							<MenuItem>
+					{drawer.map((item) => {
+						const isChild = item.children;
+						return (
+							<MenuItem
+								onClick={() => {
+									handleClick(item);
+								}}
+								key={item.id}>
 								<div className='flex items-center justify-between gap-2 text-md w-full rounded-2xl text-white py-2 px-3 hover:bg-white/10 transition-colors duration-500'>
 									<div className='flex items-center gap-2'>
-										<div className='w-2 h-2' />
+										{item.icon}
 										<div className='pt-1'>
-											<p>Album</p>
+											<p>{item.text}</p>
 										</div>
 									</div>
+									{isChild && <GoChevronDown />}
 								</div>
+								{isChild && (
+									<Menu gap='gap-0'>
+										{item.children?.map((child) => {
+											return (
+												<MenuItem
+													onClick={() => {
+														handleClick(child);
+													}}
+													key={child.id}>
+													<div className='flex items-center justify-between gap-2 text-md w-full rounded-2xl text-white py-2 px-3 hover:bg-white/10 transition-colors duration-500'>
+														<div className='flex items-center gap-2'>
+															<div className='w-2 h-2' />
+															<div className='pt-1'>
+																<p>
+																	{child.text}
+																</p>
+															</div>
+														</div>
+													</div>
+												</MenuItem>
+											);
+										})}
+									</Menu>
+								)}
 							</MenuItem>
-							<MenuItem>
-								<div className='flex items-center justify-between gap-2 text-md w-full rounded-2xl text-white py-2 px-3 hover:bg-white/10 transition-colors duration-500'>
-									<div className='flex items-center gap-2'>
-										<div className='w-2 h-2' />
-										<div className='pt-1'>
-											<p>Playlist</p>
-										</div>
-									</div>
-								</div>
-							</MenuItem>
-						</Menu>
-					</MenuItem>
-					<MenuItem>
-						<div className='flex items-center justify-between gap-2 text-md w-full rounded-2xl text-white py-2 px-3 hover:bg-white/10 transition-colors duration-500'>
-							<div className='flex items-center gap-2'>
-								<GoCopilot className='text-xl' />
-								<div className='pt-1'>
-									<p>Artists</p>
-								</div>
-							</div>
-						</div>
-					</MenuItem>
-					<MenuItem>
-						<div className='flex items-center justify-between gap-2 text-md w-full rounded-2xl text-white py-2 px-3 hover:bg-white/10 transition-colors duration-500'>
-							<div className='flex items-center gap-2'>
-								<GoBook className='text-xl' />
-								<div className='pt-1'>
-									<p>Podcast</p>
-								</div>
-							</div>
-						</div>
-					</MenuItem>
+						);
+					})}
 				</Menu>
 			</nav>
 		</aside>
